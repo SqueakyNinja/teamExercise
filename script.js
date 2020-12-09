ed9b627cf532390d67317bf33781489b8793c4c7
MattiasSimonsson <Mattias.simonsson1@gmail.com>
2020-12-09 11:08:44 +0100

    implement files from repl

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
 class Game {
-    constructor() {
-        this.player1 = new Player("göran", 1)
-        this.player2 = new Player("Sven-Erik", 1)
+	constructor() {
+		this.player1 = new Player(1)
+		this.player2 = new Player(2)
+	}
+	start() {
+		sticks.noOfSticks = 21
+		document.getElementById('stick').innerHTML = 21;   
+
+    //let playerOne = prompt('Player 1, what is your name?');
+    //let playerTwo = prompt('Player 2, what is your name?');
+    
+
+    /*if(playerOne != null) {
+    document.getElementById('player1Name').innerHTML = playerOne;
     }
+    if(playerTwo != null) {
+    document.getElementById('player2Name').innerHTML = playerTwo;
+    } */
+    
+	}
+
+	lostGame() {
+		if (sticks.noOfSticks <= 0) {
+			alert('You lost the game!');
+			document.getElementById('stick').innerHTML = 0;
+		}
+	} 
+	setTotal() {
+		event.preventDefault()
+		sticks.noOfSticks = document.getElementById("totalInput").value
+		document.getElementById('stick').innerHTML = sticks.noOfSticks
+	}
 }
 
 class Player {
-  constructor(name, id) {
-    this.name = name; 
-    this.id = id;
-  }
+	constructor(id) {
+		this.name = null;
+		this.id = id;
+	}
+	setName() {
+		event.preventDefault()
+		this.name = document.getElementById("name" + this.id).value
+		document.getElementById("player" + this.id + "Name").innerHTML = this.name
+		document.getElementById("name" + this.id).value = ""
+	}
 }
 
+
 class Stick {
-  constructor() {
-      this.sticks = 21;
-    //   this.noOfSticks?
-  }
-  removeSticks(number) {
-    sticks.sticks -= number;
-    document.getElementById('stick').textContent = sticks.sticks;
-  }
-  setAmountOfSticks() {
-    document.getElementById('stick').textContent = sticks.sticks;
-  }
+	constructor() {
+		this.noOfSticks = 21;
+
+	}
+	removeSticks(number) {
+		sticks.noOfSticks -= number;
+		document.getElementById('stick').innerHTML = sticks.noOfSticks;
+		game.lostGame();
+	}
+	setAmountOfSticks() {
+		document.getElementById('stick').innerHTML = sticks.noOfSticks;
+	}
 }
+
+let game = new Game
 
 let sticks = new Stick
 
 sticks.setAmountOfSticks();
+
+
+function again() {
+  location.reload (true)
+}
+ var switchplayer =function() {
+   
+ }
