12226325f594b626a246dc66604fc3da47fddaf8
MattiasSimonsson <Mattias.simonsson1@gmail.com>
2020-12-10 11:48:39 +0100

    fixed start and again btn

--- script.js
+++ script.js
@@ -13,25 +13,26 @@ class Game {
     );
     this.setActivePlayer();
   }
-  start() {
-    sticks.noOfSticks = 21;
-    document.getElementById("stick").innerHTML = 21;
-    document.getElementById("take1Button").disabled = false;
-    document.getElementById("take2Button").disabled = false;
-    document.getElementById("take3Button").disabled = false;
-
-    /*
+  start() {    
 		let playerOne = prompt('Player 1, what is your name?');
 		let playerTwo = prompt('Player 2, what is your name?');
-
 
     if(playerOne != null) {
     document.getElementById('player1Name').innerHTML = playerOne;
     }
     if(playerTwo != null) {
     document.getElementById('player2Name').innerHTML = playerTwo;
-    } */
-  }
+		} 	
+	}
+	
+	playAgain() {
+		sticks.noOfSticks = 21;
+		document.getElementById("stick").innerHTML = 21;
+		document.getElementById("take1Button").disabled = false;
+		document.getElementById("take2Button").disabled = false;
+		document.getElementById("take3Button").disabled = false;
+	}	
+
   setActivePlayer() {
     this.lastPlayer = document.getElementById(
       "player" + this.activePlayerNumber + "Name"
